c28273f71d26c24445ebd24db3c289c259a0f598
HighlineCars <highlinecars866@gmail.com>
2024-12-06 15:35:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require('path');
+
 require("dotenv").config();
 require("./conn/conn");
 
 const user = require("./routes/user");
 const car = require("./routes/car");
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
